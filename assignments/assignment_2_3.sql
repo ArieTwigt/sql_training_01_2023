554ba7b014747d702ed3d67c2763e193cc5240c1
Arie Twigt <ariekanarie@twigtd4.nl>
2023-01-27 10:58:53 +0100

    added assignment 2_3

---
+++ assignments/assignment_2_3.sql
@@ -0,0 +1,16 @@
+SELECT cars_2seats.brand,
+       cars_2seats.model,
+       cars_2seats.average_price / cars_2seats.seats AS price_per_seat
+FROM (
+    SELECT cars.brand,
+       cars.model,
+       cars.seats,
+       ROUND(AVG(cars.price), 2)
+             as average_price
+    FROM registered_cars AS cars
+    WHERE cars.seats = 2
+    GROUP BY 1,2,3
+) AS cars_2seats
+
+GROUP BY 1,2
+ORDER BY 3 DESC
