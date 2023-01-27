80bc20ab706e9e5125e8d0b3316ca89d480ce7dc
Arie Twigt <ariekanarie@twigtd4.nl>
2023-01-27 11:46:49 +0100

    modified 2_2

--- assignments/assignment_2_2.sql
+++ assignments/assignment_2_2.sql
@@ -1,19 +1,16 @@
 
 SELECT cars_2seats.color,
-       cars_2seats.average_price
+       ROUND(AVG(cars_2seats.average_price), 2) AS average_price 
 FROM (
 
     SELECT cars.brand,
-       cars.model,
-       cars.seats,
-       cars.color,
-       COUNT(*) AS sales,
+           cars.model,
+           cars.color,
        ROUND(AVG(cars.price), 2)
              as average_price
     FROM registered_cars AS cars
     WHERE cars.seats = 2
-    GROUP BY 1,2,3,4
-    ORDER BY average_price DESC
+    GROUP BY 1,2,3
 ) AS cars_2seats
 
 GROUP BY 1
