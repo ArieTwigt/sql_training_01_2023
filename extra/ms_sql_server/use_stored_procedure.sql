cbbc400d1eb01a74883978d9cd37f2bc2288c932
Arie Twigt <ariekanarie@twigtd4.nl>
2023-01-27 16:55:13 +0100

    added extra examples

--- extra/ms_sql_server/use_stored_procedure.sql
+++ extra/ms_sql_server/use_stored_procedure.sql
@@ -1,12 +1,12 @@
 -- Microsoft SQL Server
+
+-- TODO: Set parameter values here.
 DECLARE @RC int
 DECLARE @TopAmount int = 5
 DECLARE @MinPrice int = 500000
 DECLARE @MaxPrice int = 1500000
 
--- TODO: Set parameter values here.
-
-
+-- Using the stored procedure
 EXECUTE @RC = [dbo].[get_top_models] 
    @TopAmount
   ,@MinPrice
